e5f6578c60488b6bea1cbc94d7aa6f32b8299a01
plesoun-stein <plesoun.stein@gmail.com>
2017-09-14 18:15:27 +0200

    vsechnmo

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install rsync tcpdump screen mc  autopostgresqlbackup automysqlbackup mariadb-client mariadb-server ansible git
+apt-get -y install rsync tcpdump screen mc  autopostgresqlbackup automysqlbackup mariadb-client mariadb-server ansible git postgresql-9.6 postgresql-client-9.6 
+
 
 
 cd dirs-and-users
@@ -12,8 +13,12 @@ ansible-playbook create-users.yml
 ansible-playbook create-add-extragroups.yml
 ansible-playbook create-dirs.yml
 
+ln -s /home/srv /srv
+
 /root/wrk/create_dbs.sh
 
+
+cd dirs-and-users
 ansible-playbook create-mysqldirsadnlinks
 
 /root/wrk/import_dbs.sh
