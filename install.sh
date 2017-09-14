f3e64bcb092b0e89d3df24544132bad3aef695a1
plesoun-stein <plesoun.stein@gmail.com>
2017-09-14 18:21:11 +0200

    vsechnmo

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+mydir=$(pwd)
+
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install rsync tcpdump screen mc  autopostgresqlbackup automysqlbackup mariadb-client mariadb-server ansible git postgresql-9.6 postgresql-client-9.6 
@@ -13,12 +15,13 @@ ansible-playbook create-users.yml
 ansible-playbook create-add-extragroups.yml
 ansible-playbook create-dirs.yml
 
+rmdir /srv 
 ln -s /home/srv /srv
 
-/root/wrk/create_dbs.sh
+/root/wrk/migrace_durga/create_dbs.sh
 
 
-cd dirs-and-users
+cd $mydir/dirs-and-users
 ansible-playbook create-mysqldirsadnlinks
 
 /root/wrk/import_dbs.sh
