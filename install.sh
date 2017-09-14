6fd4fe55325d7feea1469673a180f19b89b5c92c
plesoun-stein <plesoun.stein@gmail.com>
2017-09-14 18:00:59 +0200

    test celeho

--- install.sh
+++ install.sh
@@ -4,3 +4,18 @@
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install rsync tcpdump screen mc  autopostgresqlbackup automysqlbackup mariadb-client mariadb-server ansible git
+
+
+cd dirs-and-users
+ansible-playbook create-groups.yml
+ansible-playbook create-users.yml
+ansible-playbook create-add-extragroups.yml
+ansible-playbook create-dirs.yml
+
+/root/wrk/create_dbs.sh
+
+ansible-playbook create-mysqldirsadnlinks
+
+/root/wrk/import_dbs.sh
+
+
