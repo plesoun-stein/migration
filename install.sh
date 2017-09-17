965b36b2ac04fe498df005b1661c7e7f51ef3427
plesoun-stein <plesoun.stein@gmail.com>
2017-09-17 18:11:22 +0200

    instalace sudo a mlocate

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install rsync tcpdump screen mc  autopostgresqlbackup automysqlbackup mariadb-client mariadb-server ansible git postgresql-9.6 postgresql-client-9.6  
+apt-get -y install rsync tcpdump screen mc  autopostgresqlbackup automysqlbackup mariadb-client mariadb-server ansible git postgresql-9.6 postgresql-client-9.6 mlocate sudo 
 
 systemctl enable mariadb.service
 systemctl stop mariadb.service
