e0104d76dc567849450eac001faf4cdbcab1189d
plesoun-stein <plesoun.stein@gmail.com>
2017-09-17 12:31:21 +0200

    mariadn prejmenovavat uzivatele nebudem

--- install.sh
+++ install.sh
@@ -5,11 +5,5 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install rsync tcpdump screen mc  autopostgresqlbackup automysqlbackup mariadb-client mariadb-server ansible git postgresql-9.6 postgresql-client-9.6 
 
-
-systemctl stop mariadb.service
-usermod -l maria mysql
-groupmod -n mariadb mysql
-
 systemctl enable mariadb.service
-systemctl start mariadb.service
-
+systemctl stop mariadb.service
