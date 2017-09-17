352e4b93bfdfac7ecc863dc8e7363abf4d4d8340
plesoun-stein <plesoun.stein@gmail.com>
2017-09-17 10:55:38 +0200

    zrusena instalace pro mysql, jnom ve scriptu

--- install.sh
+++ install.sh
@@ -18,12 +18,12 @@ ansible-playbook create-dirs.yml
 rmdir /srv 
 ln -s /home/srv /srv
 
-cd /root/wrk/migrace_durga
+#cd /root/wrk/migrace_durga
 ./create_dbs.sh
 
 
 cd $mydir/dirs-and-users
-ansible-playbook create-mysqldirsadnlinks
+#ansible-playbook create-mysqldirsadnlinks
 
 cd /root/wrk/migrace_durga
 ./import_dbs.sh
